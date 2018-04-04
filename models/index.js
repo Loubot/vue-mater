c4d13b3571a1ae20b98c64b980adbb43c2b2b102
Angelini <lllouis@yahoo.com>
2018-04-04 19:13:38 +0100

    Declare sequelize earlier

--- models/index.js
+++ models/index.js
@@ -8,35 +8,38 @@ var env       = process.env.NODE_ENV || 'development';
 // var config    = require(__dirname + '/..\config\config.json')[env];
 var db        = {};
 
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   var sequelize = new Sequelize('vue', 'loubot', 'pass');
-// }
-const sequelize = new Sequelize('vue', 'loubot', 'pass', {
-  host: 'localhost',
-  dialect: 'mysql',
+var sequelize = {}
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+if ( true === false ) {
+    console.log('hup')
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    console.log('hup')
+    sequelize = new Sequelize('vue', 'loubot', 'pass', {
+        host: 'localhost',
+        dialect: 'mysql',
+
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
 
 
-  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
-  operatorsAliases: false
-});
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+      // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+        operatorsAliases: false
+    }); 
+}
+
+fs.readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        var model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
