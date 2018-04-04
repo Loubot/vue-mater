1046a62f63728ed39f6cac1af44c6e887830843b
Angelini <lllouis@yahoo.com>
2018-04-04 14:38:28 +0100

    Database migration

--- server.js
+++ server.js
@@ -2,7 +2,25 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var db = {}
 app = express();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('vue', 'loubot', 'pass', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+});
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
