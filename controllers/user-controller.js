24475ad832d35a1c137cbefe3173f89901e74e6b
Angelini <lllouis@yahoo.com>
2018-04-04 18:57:00 +0100

    Fetching user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,6 +6,11 @@ var models = require( '../models' )
 module.exports.controller = function( app ) {
 	app.get( '/api/user', function( req, res ) {
 		console.log( 'user controller /api/user' )
-		res.json( 'bla' )
+
+		models.User.findOne({ where: { first_name: 'louis' } } ).then( user => {
+			console.log( user )
+			res.json( user )
+		})
+		
 	})
 }
