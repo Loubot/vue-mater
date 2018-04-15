2c7306aa400f858e78321d387a517a6d9640b749
Angelini <lllouis@yahoo.com>
2018-04-15 11:54:35 +0100

    add password

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var db = {}
 var app = express();
 var models = require('./models')
 var fs = require( 'fs' )
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
 
 
 // Initialise passport
@@ -14,13 +16,15 @@ var passport = require("passport");
 app.use( passport.initialize() )
 var strategy = require('./config/strategy')( passport )
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-
-app.use(function(req, res, next) {
-  	res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
-  	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  	next();
-});
+// app.use(function(req, res, next) {
+//   	res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+//   	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   	next();
+// });
 /* Include all express controllers */
 fs.readdirSync('./controllers').forEach(function (file) {
     if(file.substr(-3) == '.js') {
