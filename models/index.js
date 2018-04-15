6b5f858a6fcf2c9aa9a21768f7b8bc13cd16eb96
Angelini <lllouis@yahoo.com>
2018-04-15 14:46:31 +0100

    Update config for db

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require( '../config/config.json' )
+var config    = require( '../config/config.js' )
 var db        = {};
 
 var sequelize = {}
